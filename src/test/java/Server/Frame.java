d2a853974f885a7ccc25c9b1172f50ba63fdb7b3
soccerlover <984442790@qq.com>
2018-03-26 00:05:03 +0800

    1th commit
    1.初步确定Frame 中控件和布局;
    2.增加开始和结束按钮的事件监听行为;(未完成,暂时只能开始采集和循环渲染,结束采集和循环渲染)

--- src/test/java/Server/Frame.java
+++ src/test/java/Server/Frame.java
@@ -1,10 +1,15 @@
 package Server;
 
+import org.apache.log4j.Logger;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by hck on 2018/3/23.
@@ -15,37 +20,88 @@ public class Frame{
     private static JButton startButton;
     private static JButton endButton;
     private static MyLabel video;
+    private static JTextField info;
+    private static Logger logger = Logger.getLogger(Frame.class);
+    private static boolean flag = false;
     public Frame() {
 
     }
 
-    public static void init(){
+    public static void init() throws IOException {
         frame = new JFrame("hck");
-        frame.setSize(500, 500);                               // 设置窗口大小
+        frame.setSize(500, 400);                               // 设置窗口大小
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);                                  // 把窗口位置设置到屏幕中心
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);      // 当点击窗口的关闭按钮时退出程序（没有这一句，程序不会退出）
 
-        basePanel = new JPanel();
-        basePanel.setLayout(new GridBagLayout());
+        basePanel = new JPanel();                                           // 设置底层容器
+        basePanel.setLayout(new GridBagLayout());                           // 设置GirdBag布局管理器
 
-        video = new MyLabel();
-        video.setText("Video");
-        basePanel.add(video,new GBC(0,0,2,1).setFill(GBC.BOTH).setIpad(500,350).setWeight(100,100));
+        video = new MyLabel("video");                                    // 初始化视频显示区域
+        video.repaint();
+        basePanel.add(video,new GBC(0,0,6,1).setFill(GBC.BOTH).setIpad(500,350).setWeight(100,100));
 
-        startButton = new JButton("Start");
-        basePanel.add(startButton,new GBC(0,1,1,1).setFill(GBC.BOTH).setWeight(0,0));
+        startButton = new JButton("Start");                             // 初始化开始按钮
+        basePanel.add(startButton,new GBC(0,1,1,1).setFill(GBC.CENTER).setIpad(50,20).setWeight(0,0));
 
-        endButton = new JButton("end");
-        basePanel.add(endButton,new GBC(1,1,1,1).setFill(GBC.BOTH).setWeight(0,0));
+        endButton = new JButton("end");                                 //  初始化结束按钮
+        basePanel.add(endButton,new GBC(1,1,1,1).setFill(GBC.CENTER).setIpad(50,20).setWeight(0,0));
+
+        info = new JTextField();
+        info.setEditable(false);
+        basePanel.add(info,new GBC(2,1,6,1).setFill(GBC.HORIZONTAL).setWeight(0,0));
+
+        initActionListener();
+
 
         frame.add(basePanel);
+        frame.setVisible(true);
+        startRepaint();
+
     }
 
+    private static void startRepaint() {
+        for(;;){
+            if(flag){
+                video.setImage(BufferQueue.peek());
+                video.repaint();
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void initActionListener() {
+        //  添加开始按钮监听事件
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                logger.info("开始播放");
+                Scanner.startCatchScreen();
+                flag = true;
+            }
+        });
+        //  添加结束按钮监听事件
+        endButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                logger.info("结束播放");
+                Scanner.startCatchScreen();
+                flag = false;
+            }
+        });
+    }
 
 
     private static class MyLabel extends JLabel {
         private BufferedImage image;
+
+        public MyLabel(String s) {
+            super(s);
+        }
 
         public void setImage(BufferedImage img) {
             this.image = img;
@@ -53,9 +109,8 @@ public class Frame{
 
         @Override
         protected void paintComponent(Graphics g) {
-            if (image != null) {
-                g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
-            }
+                g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+
         }
     }
 
